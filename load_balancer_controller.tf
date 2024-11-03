d5ff011e45da566a66aaba047d5551e68b6fd107
devops-workshop-shahar <devops-workshop-shahar@Devopss-MacBook-Pro.local>
2024-11-03 23:22:42 +0200

    needed files to the branch

---
+++ load_balancer_controller.tf
@@ -0,0 +1,21 @@
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name 
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+}
